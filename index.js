826f63294d961090294d83645555d508be344d07
Johnny Wu <KarasAya@gmail.com>
2014-09-25 15:56:35 +0800

    update

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-var FooBar = {};
+var Path = require('path');
+var FirePath = {};
 
-function privateFunc () {
-}
-
-FooBar.foobar = function () {
-    return 'foobar';
+FirePath.slash = function ( path ) {
+    // var isExtendedLengthPath = /^\\\\\?\\/.test(path);
+    // var hasNonAscii = /[^\x00-\x80]+/.test(path);
+    // if (isExtendedLengthPath || hasNonAscii) {
+    //     return path;
+    // }
+    return path.replace(/\\/g, '/');
 };
 
 //
-module.exports = FooBar;
+var _ = {};
+var prop;
+for ( prop in Path ) {
+    _[prop] = Path[prop];
+}
+for ( prop in FirePath ) {
+    _[prop] = FirePath[prop];
+}
+module.exports = _;
--- test/test.js
+++ test/test.js
@@ -1,8 +1,29 @@
-var FooBar = require('../index');
+var FirePath = require('../index');
 
-describe('FooBar.foobar', function () {
-    it('should work for simple case', function () {
-        FooBar.foobar('foo', 'bar')
-        .should.eql('foobar');
+describe('FirePath.slash', function () {
+    it('should convert backwards-slash paths to forward slash paths', function () {
+        FirePath.slash('c:/aaaa\\bbbb')
+        .should.eql('c:/aaaa/bbbb');
+
+        FirePath.slash('c:\\aaaa\\bbbb')
+        .should.eql('c:/aaaa/bbbb');
+    });
+
+    // it('should not convert extended-length paths', function () {
+    //     var path = '\\\\?\\c:\\aaaa\\bbbb';
+    //     FirePath.slash(path)
+    //     .should.eql(path);
+    // });
+
+    it('should convert paths with Unicode', function () {
+        var path = 'c:\\aaaa\\bbbb\\★';
+        FirePath.slash(path)
+        .should.eql('c:/aaaa/bbbb/★');
+    });
+
+    it('should convert paths with Chinese', function () {
+        var path = 'c:\\aaaa\\bbbb\\你好';
+        FirePath.slash(path)
+        .should.eql('c:/aaaa/bbbb/你好');
     });
 });
