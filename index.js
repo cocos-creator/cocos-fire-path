4ed633a79e650cb51718539b0af9a9e14e3252d5
Johnny Wu <karsaaya@gmail.com>
2016-04-12 11:25:56 +0800

    path contains will consider case insensitive system

--- index.js
+++ index.js
@@ -2,31 +2,31 @@ var Path = require('path');
 var FirePath = {};
 
 FirePath.basenameNoExt = function ( path ) {
-    return Path.basename(path, Path.extname(path) );
+  return Path.basename(path, Path.extname(path) );
 };
 
 FirePath.slash = function ( path ) {
-    // var isExtendedLengthPath = /^\\\\\?\\/.test(path);
-    // var hasNonAscii = /[^\x00-\x80]+/.test(path);
-    // if (isExtendedLengthPath || hasNonAscii) {
-    //     return path;
-    // }
-    return path.replace(/\\/g, '/');
+  // var isExtendedLengthPath = /^\\\\\?\\/.test(path);
+  // var hasNonAscii = /[^\x00-\x80]+/.test(path);
+  // if (isExtendedLengthPath || hasNonAscii) {
+  //     return path;
+  // }
+  return path.replace(/\\/g, '/');
 };
 
 FirePath.stripSep = function ( path ) {
-    path = Path.normalize(path);
-    for ( var i = path.length-1; i >= 0; --i ) {
-        if ( path[i] !== Path.sep ) {
-            break;
-        }
+  path = Path.normalize(path);
+  for ( var i = path.length-1; i >= 0; --i ) {
+    if ( path[i] !== Path.sep ) {
+      break;
     }
-    return path.substring(0,i+1);
+  }
+  return path.substring(0,i+1);
 };
 
 FirePath.stripExt = function ( path ) {
-    var extname = Path.extname(path);
-    return path.substring(0, path.length-extname.length);
+  var extname = Path.extname(path);
+  return path.substring(0, path.length-extname.length);
 };
 
 // pathA = foo/bar,         pathB = foo/bar/foobar, return true
@@ -34,34 +34,39 @@ FirePath.stripExt = function ( path ) {
 // pathA = foo/bar/foobar,  pathB = foo/bar,        return false
 // pathA = foo/bar/foobar,  pathB = foobar/bar/foo, return false
 FirePath.contains = function ( pathA, pathB ) {
-    pathA = FirePath.stripSep(pathA);
-    pathB = FirePath.stripSep(pathB);
+  pathA = FirePath.stripSep(pathA);
+  pathB = FirePath.stripSep(pathB);
 
-    //
-    if ( pathA === pathB ) {
-        return true;
-    }
+  if ( process.platform === 'win32' ) {
+    pathA = pathA.toLowerCase();
+    pathB = pathB.toLowerCase();
+  }
 
-    // never compare files
-    if ( Path.dirname(pathA) === Path.dirname(pathB) ) {
-        return false;
-    }
+  //
+  if ( pathA === pathB ) {
+    return true;
+  }
 
-    if ( pathA.length < pathB.length &&
-         pathB.indexOf (pathA + Path.sep) === 0 ) {
-        return true;
-    }
-
+  // never compare files
+  if ( Path.dirname(pathA) === Path.dirname(pathB) ) {
     return false;
+  }
+
+  if ( pathA.length < pathB.length &&
+      pathB.indexOf (pathA + Path.sep) === 0 ) {
+        return true;
+      }
+
+      return false;
 };
 
 //
 var _ = {};
 var prop;
 for ( prop in Path ) {
-    _[prop] = Path[prop];
+  _[prop] = Path[prop];
 }
 for ( prop in FirePath ) {
-    _[prop] = FirePath[prop];
+  _[prop] = FirePath[prop];
 }
 module.exports = _;
--- test/test.js
+++ test/test.js
@@ -1,91 +1,96 @@
 var FirePath = require('../index');
 
 describe('FirePath.contains', function () {
-    it('should work for simple case', function () {
-        FirePath.contains('foo/bar', 'foo/bar/foobar')
-        .should.eql(true, 'foo/bar > foo/bar/foobar');
+  it('should work for simple case', function () {
+    FirePath.contains('foo/bar', 'foo/bar/foobar')
+    .should.eql(true, 'foo/bar > foo/bar/foobar');
 
-        FirePath.contains('foo/bar', 'foo/bar')
-        .should.eql(true, 'foo/bar == foo/bar');
+    FirePath.contains('foo/bar', 'foo/bar')
+    .should.eql(true, 'foo/bar == foo/bar');
 
-        FirePath.contains('foo/bar/foo-01', 'foo/bar/foo-02')
-        .should.eql(false, 'foo/bar/foo-01 != foo/bar/foo-02');
+    FirePath.contains('foo/bar/foo-01', 'foo/bar/foo-02')
+    .should.eql(false, 'foo/bar/foo-01 != foo/bar/foo-02');
 
-        FirePath.contains('foo/bar/foobar', 'foo/bar')
-        .should.eql(false, 'foo/bar/foobar < foo/bar');
+    FirePath.contains('foo/bar/foobar', 'foo/bar')
+    .should.eql(false, 'foo/bar/foobar < foo/bar');
 
-        FirePath.contains('foo/bar/foobar', 'foobar/bar/foo')
-        .should.eql(false, 'foo/bar/foobar != foobar/bar/foo');
+    FirePath.contains('foo/bar/foobar', 'foobar/bar/foo')
+    .should.eql(false, 'foo/bar/foobar != foobar/bar/foo');
 
-        FirePath.contains('foo/bar/foobar/foobar.js', 'foo/bar/foobar/foobar.js.meta')
-        .should.eql(false, 'foo/bar/foobar/foobar.js != foo/bar/foobar/foobar.js.meta');
+    FirePath.contains('foo/bar/foobar/foobar.js', 'foo/bar/foobar/foobar.js.meta')
+    .should.eql(false, 'foo/bar/foobar/foobar.js != foo/bar/foobar/foobar.js.meta');
 
-        FirePath.contains('foo/bar/foobar', 'foo/bar/foobar/foobar.js')
-        .should.eql(true, 'foo/bar/foobar > foo/bar/foobar/foobar.js' );
+    FirePath.contains('foo/bar/foobar', 'foo/bar/foobar/foobar.js')
+    .should.eql(true, 'foo/bar/foobar > foo/bar/foobar/foobar.js' );
 
-        FirePath.contains('foo/bar', 'foo/bar1/foobar')
-        .should.eql(false, 'foo/bar != foo/bar1/foobar' );
+    FirePath.contains('foo/bar', 'foo/bar1/foobar')
+    .should.eql(false, 'foo/bar != foo/bar1/foobar' );
 
-        FirePath.contains('foo/bar1/', 'foo/bar1/foobar')
-        .should.eql(true, 'foo/bar1/ == foo/bar1/foobar' );
+    FirePath.contains('foo/bar1/', 'foo/bar1/foobar')
+    .should.eql(true, 'foo/bar1/ == foo/bar1/foobar' );
 
-    });
+  });
 
-    it('should work for complex typing', function () {
-        FirePath.contains('foo/bar/', 'foo/bar/foobar')
-        .should.eql(true, 'foo/bar/ > foo/bar/foobar');
+  it('should work for complex typing', function () {
+    FirePath.contains('foo/bar/', 'foo/bar/foobar')
+    .should.eql(true, 'foo/bar/ > foo/bar/foobar');
 
-        FirePath.contains('foo/bar', 'foo/bar/')
-        .should.eql(true, 'foo/bar == foo/bar/');
+    FirePath.contains('foo/bar', 'foo/bar/')
+    .should.eql(true, 'foo/bar == foo/bar/');
 
-        FirePath.contains('foo/bar/', 'foo/bar')
-        .should.eql(true, 'foo/bar/ == foo/bar');
+    FirePath.contains('foo/bar/', 'foo/bar')
+    .should.eql(true, 'foo/bar/ == foo/bar');
 
-        FirePath.contains('foo/bar/foo-01/', 'foo/bar/foo-02')
-        .should.eql(false, 'foo/bar/foo-01/ != foo/bar/foo-02');
-    });
+    FirePath.contains('foo/bar/foo-01/', 'foo/bar/foo-02')
+    .should.eql(false, 'foo/bar/foo-01/ != foo/bar/foo-02');
+  });
+
+  it('should work for case insensitive system', function () {
+    FirePath.contains('c:\\foo\\bar', 'C:\\Foo\\Bar')
+    .should.eql(process.platform === 'win32', 'c:\\foo\\bar === C:\\Foo\\Bar in Windows');
+  });
 });
 
 describe('FirePath.slash', function () {
-    it('should convert backwards-slash paths to forward slash paths', function () {
-        FirePath.slash('c:/aaaa\\bbbb')
-        .should.eql('c:/aaaa/bbbb');
+  it('should convert backwards-slash paths to forward slash paths', function () {
+    FirePath.slash('c:/aaaa\\bbbb')
+    .should.eql('c:/aaaa/bbbb');
 
-        FirePath.slash('c:\\aaaa\\bbbb')
-        .should.eql('c:/aaaa/bbbb');
-    });
+    FirePath.slash('c:\\aaaa\\bbbb')
+    .should.eql('c:/aaaa/bbbb');
+  });
 
-    // it('should not convert extended-length paths', function () {
-    //     var path = '\\\\?\\c:\\aaaa\\bbbb';
-    //     FirePath.slash(path)
-    //     .should.eql(path);
-    // });
+  // it('should not convert extended-length paths', function () {
+  //     var path = '\\\\?\\c:\\aaaa\\bbbb';
+  //     FirePath.slash(path)
+  //     .should.eql(path);
+  // });
 
-    it('should convert paths with Unicode', function () {
-        var path = 'c:\\aaaa\\bbbb\\★';
-        FirePath.slash(path)
-        .should.eql('c:/aaaa/bbbb/★');
-    });
+  it('should convert paths with Unicode', function () {
+    var path = 'c:\\aaaa\\bbbb\\★';
+    FirePath.slash(path)
+    .should.eql('c:/aaaa/bbbb/★');
+  });
 
-    it('should convert paths with Chinese', function () {
-        var path = 'c:\\aaaa\\bbbb\\你好';
-        FirePath.slash(path)
-        .should.eql('c:/aaaa/bbbb/你好');
-    });
+  it('should convert paths with Chinese', function () {
+    var path = 'c:\\aaaa\\bbbb\\你好';
+    FirePath.slash(path)
+    .should.eql('c:/aaaa/bbbb/你好');
+  });
 });
 
 describe('FirePath.stripExt', function () {
-    it('should work for simple case', function () {
-        FirePath.stripExt('foo/bar/foobar.sprite')
-        .should.eql('foo/bar/foobar');
+  it('should work for simple case', function () {
+    FirePath.stripExt('foo/bar/foobar.sprite')
+    .should.eql('foo/bar/foobar');
 
-        FirePath.stripExt('foo/bar/foobar')
-        .should.eql('foo/bar/foobar');
+    FirePath.stripExt('foo/bar/foobar')
+    .should.eql('foo/bar/foobar');
 
-        FirePath.stripExt('foo/bar/foobar.sprite.foo')
-        .should.eql('foo/bar/foobar.sprite');
+    FirePath.stripExt('foo/bar/foobar.sprite.foo')
+    .should.eql('foo/bar/foobar.sprite');
 
-        FirePath.stripExt('foo/bar/foobar.png/hello.sprite')
-        .should.eql('foo/bar/foobar.png/hello');
-    });
+    FirePath.stripExt('foo/bar/foobar.png/hello.sprite')
+    .should.eql('foo/bar/foobar.png/hello');
+  });
 });
